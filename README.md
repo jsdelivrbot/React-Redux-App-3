e1b0ee33b11917db1bf243311a70db3d13fdbb99
Kamil <kamild01@gmail.com>
2017-11-27 09:48:13 +0100

    Added first reducer

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Simple React Redux app 
+
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'JS: Good parts' },
+    { title: 'Harry Potter' },
+    { title: 'Władca Pierścieni' },
+    { title: 'You dont know JS' }
+  ];
+}
